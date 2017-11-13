96bcde982a9f9850274ea348aa278774b4e7e002
Sinapsel <marksimanovskiy@gmail.com>
2017-11-13 19:42:40 +0300

    Plus and Minus methods added

--- app/src/main/java/com/stuuuuuudent6/dacalc/MainActivity.java
+++ app/src/main/java/com/stuuuuuudent6/dacalc/MainActivity.java
@@ -1,7 +1,11 @@
 package com.stuuuuuudent6.dacalc;
 
+import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void plus(View v){
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String num1Text = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String num2Text = num2.getText().toString();
+        TextView res = (TextView) this.findViewById(R.id.out);
+        res.setText(String.valueOf(Double.parseDouble(num1Text) + Double.parseDouble(num2Text)));
+
+    }
+
+    public void minus(View v) {
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String num1Text = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String num2Text = num2.getText().toString();
+        TextView res = (TextView) this.findViewById(R.id.out);
+        res.setText(String.valueOf(Double.parseDouble(num1Text) - Double.parseDouble(num2Text)));
     }
 }
